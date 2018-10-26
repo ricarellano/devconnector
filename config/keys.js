1b568bdbe4ca773dd981f82736a950e51c3def2b
ricarellano <rarellanob@gmail.com>
2018-10-25 22:46:15 -0500

    User API Routes & JWT Authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://ricarellano:devconnector1@ds141043.mlab.com:41043/devconnector"
+    "mongodb://ricarellano:devconnector1@ds141043.mlab.com:41043/devconnector",
+  secretOrKey: "secret"
 };
