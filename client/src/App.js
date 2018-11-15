f807c9ef46a9fb8caaa2084891cd41fb4f65f572
ricarellano <rarellanob@gmail.com>
2018-11-14 22:19:57 -0600

    setup react

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+import NavBar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
