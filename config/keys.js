2158281726b891411990116a73f3161297f13f58
ricarellano <rarellanob@gmail.com>
2018-12-28 17:33:54 -0600

    prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://ricarellano:devconnector1@ds141043.mlab.com:41043/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
